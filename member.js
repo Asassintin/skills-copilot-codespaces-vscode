3086b1fc4a02974473fd3b2bd45f738d646da9f1
Miguel Angel Agustin Zuñiga Romo <miguel.zuniga0668@alumnos.udg.mx>
2025-08-30 23:33:22 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+  return [
+    { id: 1, name: "JavaScript", level: "Expert" },
+    { id: 2, name: "Python", level: "Intermediate" },
+    { id: 3, name: "Java", level: "Beginner" }
+  ];
+}
